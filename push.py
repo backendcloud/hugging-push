5527ce8d027a1e552b18ee41c367644fc1d68d35
hanwei <pyartist@outlook.com>
2023-04-24 17:36:39 +0800

    modify

--- push.py
+++ push.py
@@ -4,6 +4,7 @@ from huggingface_hub import create_repo, upload_folder, whoami
 def main(
     repo_id: str,
     github_repo: str,
+    subdirectory: str,
     token: str,
     repo_type: str = "space",
     space_sdk: str = "gradio",
@@ -29,7 +30,7 @@ def main(
     print(f"\t- Repo URL: {url}")
 
     latter_repo = github_repo.split("/")[1]
-    directory = f"{latter_repo}/{latter_repo}"
+    directory = f"{latter_repo}/{latter_repo}/{subdirectory}"
 
     # Sync folder
     commit_url = upload_folder(
